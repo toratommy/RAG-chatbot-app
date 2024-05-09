8407d89f15b62ccd191832f5db3dabfcf4e78917
toratommy <tommy@torainsights.ai>
2024-05-09 14:55:42 -0500

    change structure

--- app.py
+++ app.py
@@ -1,54 +1,99 @@
 import streamlit as st
 import os
-from src.utils import create_dir_if_not_exists, fetch_articles, load_documents_and_prepare_index, query_response, make_article_selections
+from src.utils import *
 
 # TO DO:
-# sequential chat messages (check latest streamlit features)
-# add memory
-# let user select local folder OR cloud folder
+
+# let users select models (add placeholder for privately hosted)
+# let agent render plots
+# include confidence scores for each response and references to documents
 
 def main():
-    # set theme
+    # Set theme
     st._config.set_option(f'theme.base' ,"light" )
     st._config.set_option(f'theme.primaryColor',"#f63366")
     st._config.set_option(f'theme.backgroundColor',"#FFFFFF")
     st._config.set_option(f'theme.secondaryBackgroundColor', "#f0f2f6")
     st._config.set_option(f'theme.textColor',"#262730")
-
-    st.set_page_config(page_title="Chat with Company Articles", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
-    st.title("Company News Chat - Powered by LlamaIndex and OpenAI")
-
+    # Setting up the Streamlit page configuration
+    st.set_page_config(page_title="Chat with Your Knowledge Base", page_icon="🦙", layout="centered", initial_sidebar_state="auto")
+    
+    st.title("Personal Knowledge Base Agent")
+    st.caption("RAG Powered by LlamaIndex and Pathway")
+    st.divider()
+    
+    # Create a 'data' directory if it doesn't already exist
     create_dir_if_not_exists('data')
-
-    # Sidebar for specifying parameters for fetching articles
+    
+    # Sidebar for selecting the knowledge base source
     with st.sidebar:
+        # Display logo in sidebar
         st.image('https://assets-global.website-files.com/659c81c957e77aeea1809418/65b2f184ee9f42f63bc2c651_TORA%20Logo%20(No%20Background)-p-800.png', width=225)
-        st.title("Fetch Company Articles")
-        company_name, start_date, end_date = make_article_selections()
-        fetch_and_index_articles = st.button("Fetch and Index Articles")
-
-    if fetch_and_index_articles:
-        api_key = st.secrets["news_api"]
-        company_folder = os.path.join('data', company_name.replace(" ", "_"))  # Normalize folder name
-        create_dir_if_not_exists(company_folder)
-        articles_fetched = fetch_articles(company_name, start_date, end_date, api_key, company_folder)
-        if articles_fetched:
-            st.success(f"Articles for {company_name} successfully fetched and saved in {company_folder}.")
-            with st.spinner('Indexing documents...'):
-                index = load_documents_and_prepare_index(company_folder)
-            st.session_state['index'] = index
+        st.header('About')
+        st.caption('''Welcome to the Personal Knowledge Base Agent, powered by LlamaIndex and RAG (Retrieval-Augmented Generation) technology. 
+                   This interactive platform allows you to seamlessly fetch, index, and query a vast array of documents, 
+                   enhancing your ability to quickly access and utilize information. Whether you're integrating local data, 
+                   connecting to a cloud document management system, or fetching public articles, 
+                   our tool is designed to streamline your information management processes, 
+                   making knowledge discovery as intuitive as having a conversation.''')
+        st.divider()
+        st.header("Select your Knowledge Base")
+        knowledge_base_type = make_knowledge_base_selections()
+        
+        # Handling public data fetching
+        if knowledge_base_type == 'Fetch Public Data':
+            company_name, start_date, end_date, file_path = make_public_document_selections()
+            fetch_documents = st.button("Fetch and Load Documents")
+            if fetch_documents:
+                api_key = st.secrets["news_api"]
+                create_dir_if_not_exists(file_path)
+                fetched_status = fetch_public_documents(company_name, start_date, end_date, api_key, file_path)
+                if fetched_status:
+                    num_docs = count_files_in_directory(file_path)
+                    st.success(f"{num_docs} documents for {company_name} successfully fetched and saved in {file_path}.")
+                    st.session_state['path_saved'] = True
+                else:
+                    st.error("Failed to fetch articles. Please check the company name or try again later.")
         else:
-            st.error("Failed to fetch articles. Please check the company name or try again later.")
+            # Handle local or cloud directory path input
+            file_path = enter_path()
+            st.session_state['path_saved'] = st.button('Save File Path')
+            if st.session_state['path_saved']:
+                num_docs = count_files_in_directory(file_path)
+                st.success(f'Path has been sucessfully saved. {num_docs} documents are available in {file_path}')
+
+    # Indexing control
+    if (file_path != ''):
+        st.subheader('Index Your Knowledge Base')
+        st.caption('Indexing is the process of organizing a vast amount of text data in a way that allows the RAG system to quickly find the most relevant pieces of information for a given query.')
+        st.markdown(f'Current Knowledge Base Directory: `{file_path}`')
+        index_documents = st.button('Index Knowledge Base')
+        if index_documents:
+            with st.spinner('Indexing documents...'):
+                index, files_df = load_documents_and_prepare_index(file_path)
+                st.session_state['index'] = index
+                num_docs = count_files_in_directory(file_path)
+                st.success(f"{num_docs} documents successfully indexed. Start a chat with your knowledge base agent below!")
+                st.dataframe(files_df)
+                # Reset chat upon new indexing
+                st.session_state.messages = [{"role": "assistant", "content": f"Hello! I am your personal knowledge base agent. Ask me anything about your knowledge base. I currently have access to {num_docs} indexed files in from following directory: `{file_path}`"}]
 
     # Main chat interface
-    st.header("Interactive Chat")
+    st.subheader("Interactive Chat")
     if 'index' in st.session_state:
-        user_query = st.text_input("Enter your question:")
-        if user_query:
-            response = query_response(st.session_state['index'], user_query)
-            st.text_area("Response", response, height=150)
+        user_input = st.chat_input("Your question:")
+        if user_input:
+            if "messages" not in st.session_state:
+                st.session_state.messages = []
+            st.session_state.messages.append({"role": "user", "content": format_message(user_input)})
+            response = query_response(st.session_state['index'], user_input).response
+            st.session_state.messages.append({"role": "assistant", "content": format_message(response)})
+
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.write(message["content"])
     else:
-        st.warning("No indexed data available for chat. Please fetch and prepare articles first.")
+        st.warning("No indexed data available for chat. Please use the left control panel to connect a knowledge base and click the button above to index your documents.")
 
 if __name__ == "__main__":
     main()
--- src/utils.py
+++ src/utils.py
@@ -1,39 +1,65 @@
 import requests
 import os
+import pandas as pd
 import streamlit as st
 from datetime import datetime, timedelta
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, ServiceContext
 from llama_index.llms.openai import OpenAI
 import openai
+from tika import parser as tika_parser  # Apache Tika for parsing various document types
 
-@st.experimental_fragment
-def make_article_selections():
-    company_name = st.text_input("Enter a company name:")
+def format_message(content):
+    # Escape dollar signs to prevent Markdown formatting issues
+    return content.replace("$", "\$")
+
+def make_knowledge_base_selections():
+    knowledge_base_type = st.selectbox('Select knowledge base type',['Local Directory','SharePoint','Google Drive','S3 Bucket','Fetch Public Data'])
+    return knowledge_base_type
+
+def enter_path():
+    file_path = st.text_input('Enter file path', placeholder='/Users/YourName/Documents')
+    return file_path
+
+def make_public_document_selections():
+    company_name = st.text_input("Enter a company name")
     today = datetime.now()
     one_month_ago = today - timedelta(days=30)
-    start_date = st.date_input("Start Date", value=one_month_ago, min_value=one_month_ago, max_value=today)
-    end_date = st.date_input("End Date", value=today, min_value=one_month_ago, max_value=today)
-    return company_name, start_date, end_date
+    start_date = st.date_input("Start date", value=one_month_ago, min_value=one_month_ago, max_value=today)
+    end_date = st.date_input("End date", value=today, min_value=one_month_ago, max_value=today)
+    file_path = st.text_input('Enter file path to store fetched documents', value=os.path.join('data', company_name.replace(" ", "_")) )
+    return company_name, start_date, end_date, file_path
 
-#@st.cache_resource(show_spinner=True)
+@st.cache_resource(show_spinner=True)
 def set_openai_key():
     openai.api_key = st.secrets["openai_api_key"]
 
+def count_files_in_directory(directory):
+    count = 0
+    for root, dirs, files in os.walk(directory):
+        count += len(files)
+    return count
+
 @st.cache_data(show_spinner=False)
-def fetch_articles(company_name, start_date, end_date, api_key, save_path='data'):
+def fetch_public_documents(company_name, start_date, end_date, api_key, save_path='data'):
     start_date_str = start_date.strftime('%Y-%m-%d')
     end_date_str = end_date.strftime('%Y-%m-%d')
     url = f"https://newsapi.org/v2/everything?q={company_name}&from={start_date_str}&to={end_date_str}&sortBy=publishedAt&apiKey={api_key}"
     response = requests.get(url)
     if response.status_code == 200:
-        articles = response.json()['articles']
-        for i, article in enumerate(articles):
+        documents = response.json()['articles']
+        for i, article in enumerate(documents):
+            title = article.get('title', 'No Title Provided')
+            publish_date = article.get('publishedAt', 'No Date Provided').split('T')[0]  # Extract just the date part
+            publisher = article['source'].get('name', 'Unknown Publisher').replace(' ', '_').replace('/', '_')  # Replace spaces and slashes to avoid issues in filenames
             content = article.get('content')
             if content:
-                file_name = f"{datetime.now().strftime('%Y%m%d%H%M%S')}_{i}.txt"
+                # Format the document with title and date before the content
+                document_content = f"Title: {title}\nDate: {publish_date}\n\n{content}"
+                # Format the file name to include the publisher name and date
+                file_name = f"{publisher}_{publish_date}_{datetime.now().strftime('%Y%m%d%H%M%S')}_{i}.txt"
                 file_path = os.path.join(save_path, file_name)
                 with open(file_path, 'w') as file:
-                    file.write(content)
+                    file.write(document_content)
         return True
     else:
         return False
@@ -48,17 +74,40 @@ def get_model_and_tokenizer():
     llm = OpenAI(api_key=openai.api_key, model="gpt-3.5-turbo")
     return llm
 
-#@st.cache_data(ttl=86400)  # Cache for one day
+def get_meta(file_path):
+    """ Custom function to extract metadata from files """
+    return {
+        "file_path": file_path,
+        "last_modified": os.path.getmtime(file_path)
+    }
+
+@st.cache_data(ttl=86400)  # Cache for one day
 def load_documents_and_prepare_index(directory):
-    reader = SimpleDirectoryReader(directory)
+    llm = get_model_and_tokenizer()
+    service_context = ServiceContext.from_defaults(chunk_size=1024, llm=llm)
+
+    # Use SimpleDirectoryReader with custom metadata extraction
+    reader = SimpleDirectoryReader(directory, recursive=True, file_metadata=get_meta)
     documents = reader.load_data()
 
-    llm = get_model_and_tokenizer()
-
-    service_context = ServiceContext.from_defaults(chunk_size=1024, llm=llm)
+    # Create the index from the collected documents
     index = VectorStoreIndex.from_documents(documents, service_context=service_context)
 
-    return index
+    # Generate data for DataFrame about the documents, using metadata
+    data = []
+    for doc in documents:
+        doc_path = doc.metadata['file_path']
+        last_modified = doc.metadata['last_modified']
+        data.append({
+            "file_name": os.path.basename(doc_path),
+            "folder_name": os.path.basename(os.path.dirname(doc_path)),
+            "file_type": os.path.splitext(doc_path)[1][1:],  # Remove the dot from extension
+            "last_updated_date": datetime.fromtimestamp(last_modified).strftime('%Y-%m-%d %H:%M:%S'),
+            "indexed_status": "Yes"
+        })
+
+    df = pd.DataFrame(data)
+    return index, df
 
 def query_response(index, query):
     query_engine = index.as_query_engine()
